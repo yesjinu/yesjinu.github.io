432683dfaf10979d9cc996301a826fc08dce1e56
Jinu Noh <yes.jinu@gmail.com>
2022-10-09 16:36:27 +0900

    feat: change profile picture from random person to me

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,9 +36,9 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
+        src="../images/jinu-bio.jpg"
+        width={100}
+        height={100}
         quality={95}
         alt="Profile picture"
       />
