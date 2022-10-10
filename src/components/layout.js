a6857069698f14fac3a01924180f352ba4c66024
Jinu Noh <yes.jinu@gmail.com>
2022-10-10 15:05:10 +0900

    feat: in footer, remove default css setting and replace with tailwind css

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,30 +4,29 @@ import { Link } from "gatsby"
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
-
+  const header = isRootPath ? (
+    <h1 className="main-heading">
+      <Link to="/">{title}</Link>
+    </h1>
+  ) : (
+    <Link className="header-link-home" to="/">
+      {title}
+    </Link>
+  )
+  
+  
+  
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
+    <div className="max-w-screen-md mx-auto px-6" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      <footer className="sm:flex-col flex justify-center">
+        Designed by Jinu
+        <div>
+          <Link to="">Resume</Link>
+          <Link to="">Github</Link>
+          <Link to="">LinkedIn</Link>
+        </div>
       </footer>
     </div>
   )
