319c57d9501d09282dcb60ca360c8d7f1616c219
Jinu Noh <yes.jinu@gmail.com>
2022-10-10 15:22:06 +0900

    feat: style footer with flex

--- src/components/layout.js
+++ src/components/layout.js
@@ -17,17 +17,19 @@ const Layout = ({ location, title, children }) => {
   
   
   return (
-    <div className="max-w-screen-md mx-auto px-6" data-is-root-path={isRootPath}>
+    <div className="max-w-screen-md mx-auto px-8 py-6">
       <header className="global-header">{header}</header>
       <main>{children}</main>
-      <footer className="sm:flex-col flex justify-center">
-        Designed by Jinu
-        <div>
-          <Link to="">Resume</Link>
-          <Link to="">Github</Link>
-          <Link to="">LinkedIn</Link>
-        </div>
-      </footer>
+        <footer className="flex flex-col items-center justify-center mt-12 space-x-6 md:flex-row">
+          <p>
+            Designed by Jinu
+          </p>
+          <div className="flex space-x-2 text-gray">
+            <Link to="">Resume</Link>
+            <Link to="">Github</Link>
+            <Link to="">LinkedIn</Link>
+          </div>
+        </footer>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],  theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF8036",
+        secondary: "#FFD154",
+        tertiary: "#A9BED0",
+        dark: "#212121",
+        white: "#FFFFFF",
+        gray: "#737D8D",
+      },
+    },
+    screens: {
+      sm: "600px",
+      md: "960px",
+      lg: "1280px",
+    },
   },
   plugins: [],
 }
