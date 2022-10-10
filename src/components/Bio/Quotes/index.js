501987c2596a28a41afee5f466194c969fae544b
Jinu Noh <yes.jinu@gmail.com>
2022-10-10 17:50:08 +0900

    refactor: move all quotes text into Quotes components

--- src/components/Bio/Quotes/index.js
+++ src/components/Bio/Quotes/index.js
@@ -1,23 +1,17 @@
 import * as React from "react";
 
 const Quotes = () => {
+  const QUOTES_LIST = [
+    "The best way to predict the future is to invent it.",
+    "The future belongs to those who believe in the beauty of their dreams.",
+    "The future is already here — it’s just not evenly distributed.",
+    "The future is not a gift, it is an achievement.",
+    "The future is not something we enter. The future is something we create.",
+  ]
+  
   const RandomQuote = () => {
-    const random = Math.floor(Math.random() * 5);
-    
-    switch (random) {
-      case 0:
-        return (<>Take chances, make mistakes, get messy.</>);
-      case 1:
-        return (<>The future belongs to those who believe in the beauty of their dreams.</>);
-      case 2:
-        return (<>The future is already here — it’s just not evenly distributed.</>);
-      case 3:
-        return (<>The future is not a gift, it is an achievement.</>);
-      case 4:
-        return (<>The future is not something we enter. The future is something we create.</>);
-      default:
-        return (<>The future is not something we enter. The future is something we create.</>);
-    }
+    const random = Math.floor(Math.random() * QUOTES_LIST.length);
+    return QUOTES_LIST[random];
   }
   
   return (
