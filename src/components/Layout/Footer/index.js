696c57768d072c2f97c2e84cfa2eabb749f9b7bb
Jinu Noh <yes.jinu@gmail.com>
2022-10-10 15:58:56 +0900

    refactor: extract value from queried result

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -19,14 +19,18 @@ const Footer = () => {
     }
   `)
   
+  const authorName = data.site.siteMetadata?.author?.name;
+  const resumeLink = data.site.siteMetadata?.url?.resume;
+  const githubLink = data.site.siteMetadata?.url?.github;
+  const linkedinLink = data.site.siteMetadata?.url?.linkedin;
   
   return (
     <footer className="flex flex-col items-center justify-center mt-12 md:space-x-6 md:flex-row">
-      <p>Designed by {data.site.siteMetadata?.author?.name}</p>
+      <p>Designed by {authorName}</p>
       <div className="flex space-x-2 text-gray">
-        <Link to={data.site.siteMetadata?.url?.resume} target="_blank">Resume</Link>
-        <Link to={data.site.siteMetadata?.url?.github} target="_blank">Github</Link>
-        <Link to={data.site.siteMetadata?.url?.linkedin} target="_blank">LinkedIn</Link>
+        <Link to={resumeLink} target="_blank">Resume</Link>
+        <Link to={githubLink} target="_blank">GitHub</Link>
+        <Link to={linkedinLink} target="_blank">LinkedIn</Link>
       </div>
     </footer>
   )
